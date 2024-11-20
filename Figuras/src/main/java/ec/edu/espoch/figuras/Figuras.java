7dd516ac0cbc5d75401c7c58f6fbe4636fbf51a1
JhxnierCT <jhonier.cotaya02@gmail.com>
2024-11-20 13:45:28 -0500

    figuras

---
+++ Figuras/src/main/java/ec/edu/espoch/figuras/Figuras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.figuras;
+
+/**
+ *
+ * @author SO-LAB-PC5
+ */
+public class Figuras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
